76bf05e4005dcec63f66dba6767929f7278fc814
sandro <sandrus88@hotmail.it>
2021-05-30 10:34:53 +0200

    bug about load page resolved

--- crud/crud.js
+++ crud/crud.js
@@ -1,53 +1,56 @@
 var users = [
-  {
-    id: 1,
-    name: "Bob",
-    address: "Manila",
-    age: 27
-  },
-  {
-    id: 2,
-    name: "Harry",
-    address: "Baguio",
-    age: 32
-  }
+	{
+		id: 1,
+		name: "Bob",
+		address: "Manila",
+		age: 27
+	},
+	{
+		id: 2,
+		name: "Harry",
+		address: "Baguio",
+		age: 32
+	}
 ];
 
-$.each(users, function(i, user) {
-  appendToUsrTable(user);
-});
+$(document).ready(function() {
 
-$("form").submit(function(e) {
-  e.preventDefault();
-});
+	$.each(users, function(i, user) {
+		appendToUsrTable(user);
+	});
 
-$("form#addUser").submit(function() {
-  var user = {};
-  var nameInput = $('input[name="name"]').val().trim();
-  var addressInput = $('input[name="address"]').val().trim();
-  var ageInput = $('input[name="age"]').val().trim();
-  if (nameInput && addressInput && ageInput) {
-    $(this).serializeArray().map(function(data) {
-      user[data.name] = data.value;
-    });
-    var lastUser = users[Object.keys(users).sort().pop()];
-    user.id = lastUser.id + 1;
+	$("form").submit(function(e) {
+		e.preventDefault();
+	});
 
-    addUser(user);
-  } else {
-    alert("All fields must have a valid value.");
-  }
+	$("form#addUser").submit(function() {
+		var user = {};
+		var nameInput = $('input[name="name"]').val().trim();
+		var addressInput = $('input[name="address"]').val().trim();
+		var ageInput = $('input[name="age"]').val().trim();
+		if (nameInput && addressInput && ageInput) {
+			$(this).serializeArray().map(function(data) {
+				user[data.name] = data.value;
+			});
+			var lastUser = users[Object.keys(users).sort().pop()];
+			user.id = lastUser.id + 1;
+
+			addUser(user);
+		} else {
+			alert("All fields must have a valid value.");
+		}
+	});
 });
 
 function addUser(user) {
-  users.push(user);
-  appendToUsrTable(user);
+	users.push(user);
+	appendToUsrTable(user);
 }
 
 function editUser(id) {
-  users.forEach(function(user, i) {
-    if (user.id == id) {
-      $(".modal-body").empty().append(`
+	users.forEach(function(user, i) {
+		if (user.id == id) {
+			$(".modal-body").empty().append(`
                 <form id="updateUser" action="">
                     <label for="name">Name</label>
                     <input class="form-control" type="text" name="name" value="${user.name}"/>
@@ -56,71 +59,71 @@ function editUser(id) {
                     <label for="age">Age</label>
                     <input class="form-control" type="number" name="age" value="${user.age}" min=10 max=100/>
             `);
-      $(".modal-footer").empty().append(`
+			$(".modal-footer").empty().append(`
                     <button type="button" type="submit" class="btn btn-primary" onClick="updateUser(${id})">Save changes</button>
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 </form>
             `);
-    }
-  });
+		}
+	});
 }
 
 function deleteUser(id) {
-  var action = confirm("Are you sure you want to delete this user?");
-  var msg = "User deleted successfully!";
-  users.forEach(function(user, i) {
-    if (user.id == id && action != false) {
-      users.splice(i, 1);
-      $("#userTable #user-" + user.id).remove();
-      flashMessage(msg);
-    }
-  });
+	var action = confirm("Are you sure you want to delete this user?");
+	var msg = "User deleted successfully!";
+	users.forEach(function(user, i) {
+		if (user.id == id && action != false) {
+			users.splice(i, 1);
+			$("#userTable #user-" + user.id).remove();
+			flashMessage(msg);
+		}
+	});
 }
 
 function updateUser(id) {
-  var msg = "User updated successfully!";
-  var user = {};
-  user.id = id;
-  users.forEach(function(user, i) {
-    if (user.id == id) {
-      $("#updateUser").children("input").each(function() {
-        var value = $(this).val();
-        var attr = $(this).attr("name");
-        if (attr == "name") {
-          user.name = value;
-        } else if (attr == "address") {
-          user.address = value;
-        } else if (attr == "age") {
-          user.age = value;
-        }
-      });
-      users.splice(i, 1);
-      users.splice(user.id - 1, 0, user);
-      $("#userTable #user-" + user.id).children(".userData").each(function() {
-        var attr = $(this).attr("name");
-        if (attr == "name") {
-          $(this).text(user.name);
-        } else if (attr == "address") {
-          $(this).text(user.address);
-        } else {
-          $(this).text(user.age);
-        }
-      });
-      $(".modal").modal("toggle");
-      flashMessage(msg);
-    }
-  });
+	var msg = "User updated successfully!";
+	var user = {};
+	user.id = id;
+	users.forEach(function(user, i) {
+		if (user.id == id) {
+			$("#updateUser").children("input").each(function() {
+				var value = $(this).val();
+				var attr = $(this).attr("name");
+				if (attr == "name") {
+					user.name = value;
+				} else if (attr == "address") {
+					user.address = value;
+				} else if (attr == "age") {
+					user.age = value;
+				}
+			});
+			users.splice(i, 1);
+			users.splice(user.id - 1, 0, user);
+			$("#userTable #user-" + user.id).children(".userData").each(function() {
+				var attr = $(this).attr("name");
+				if (attr == "name") {
+					$(this).text(user.name);
+				} else if (attr == "address") {
+					$(this).text(user.address);
+				} else {
+					$(this).text(user.age);
+				}
+			});
+			$(".modal").modal("toggle");
+			flashMessage(msg);
+		}
+	});
 }
 
 function flashMessage(msg) {
-  $(".flashMsg").remove();
-  $(".row").prepend(`
+	$(".flashMsg").remove();
+	$(".row").prepend(`
         <div class="col-sm-12"><div class="flashMsg alert alert-success alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> <strong>${msg}</strong></div></div>
     `);
 }
 
 function appendToUsrTable(user) {
-  $("#userTable > tbody:last-child").append(`
+	$("#userTable > tbody:last-child").append(`
         <tr id="user-${user.id}">
             <td class="userData" name="name">${user.name}</td>
             '<td class="userData" name="address">${user.address}</td>
